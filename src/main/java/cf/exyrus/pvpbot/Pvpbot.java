661621a40f73cc3d42df7d319d5e17642f278465
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-05-05 01:39:04 +0530

    Add files via upload

---
+++ src/main/java/cf/exyrus/pvpbot/Pvpbot.java
@@ -0,0 +1,17 @@
+package cf.exyrus.pvpbot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pvpbot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
